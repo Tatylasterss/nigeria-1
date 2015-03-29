7282f70d69864800ddd1bf9a1e60516b88aac6c5
Kuan Butts <kuanbutts@yahoo.com>
2015-03-29 17:30:49 -0400

    add leaflet on div

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var leaflet = require('leaflet');
-var heat = require('leaflet.heat');
-alert('hello');
+var heat    = require('leaflet.heat');
+
+// var mapdiv  = document.getElementById('map');
+var map    = L.map('map').setView([9.695584, 7.644742], 6);
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+var osm    = new L.TileLayer(osmUrl);
+osm.addTo(map);
+
+
+
